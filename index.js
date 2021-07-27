1f60aeabab583110bb150421834ed8b5b0a98ce6
It5Me <56817602+It5Me@users.noreply.github.com>
2021-07-28 00:58:37 +0700

    line

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 require('dotenv').config({ path: './config.env' });
 const config = require('./src/config');
 const express = require('express');
+const passport = require('passport');
+const LineStrategy = require('passport-line').Strategy;
 const connectDB = require('./src/config/db');
 
 connectDB();
 const app = express();
 const RestaurantRoute = require('./src/routes/RestaurantRoute');
-
-//when use req.body
 app.use(express.json());
-// app.post('/testserver', (req, res) => {
-//     res.send('hellopim');
-// });
+
+passport.use(
+    new LineStrategy(
+        {
+            channelID: config.LINE_CHANNEL_ID,
+            channelSecret: config.LINE_CHANNEL_SECRET,
+            callbackURL:
+                'https://shrouded-plains-45055.herokuapp.com/auth/line/callback',
+        },
+        function (accessToken, refreshToken, profile, done) {
+            // asynchronous verification, for effect...
+            process.nextTick(function () {
+                // To keep the example simple, the user's LINE profile is returned to
+                // represent the logged-in user.  In a typical application, you would want
+                // to associate the LINE account with a user record in your database,
+                // and return that user instead.
+                return done(null, profile);
+            });
+        }
+    )
+);
+//when use req.body
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/auth/line', passport.authenticate('line'), function (req, res) {
+    // The request will be redirected to LINE for authentication, so this
+    // function will not be called.
+});
+app.get(
+    '/auth/line/callback',
+    passport.authenticate('line', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/');
+    }
+);
 app.use(RestaurantRoute);
 app.listen(config.PORT, () =>
     console.log(`Server RUNNING ON PORT ${config.PORT}`)
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
     MONGODB_URI: process.env.MONGODB_URI,
+    LINE_CHANNEL_ID: process.env.LINE_CHANNEL_ID,
+    LINE_CHANNEL_SECRET: process.env.LINE_CHANNEL_SECRET,
 };
