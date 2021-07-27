5e6a3a915daa8c06b58bd5d1420834a1dd6e5492
It5Me <56817602+It5Me@users.noreply.github.com>
2021-07-28 01:20:03 +0700

    new

--- index.js
+++ index.js
@@ -12,43 +12,43 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(
-    new LineStrategy(
-        {
-            channelID: config.LINE_CHANNEL_ID,
-            channelSecret: config.LINE_CHANNEL_SECRET,
-            callbackURL:
-                'https://shrouded-plains-45055.herokuapp.com/auth/line/callback',
-        },
-        function (accessToken, refreshToken, profile, done) {
-            // asynchronous verification, for effect...
-            // process.nextTick(function () {
-            //     // To keep the example simple, the user's LINE profile is returned to
-            //     // represent the logged-in user.  In a typical application, you would want
-            //     // to associate the LINE account with a user record in your database,
-            //     // and return that user instead.
-            //     return done(null, profile);
-            // });
-            console.log('accesToken', accessToken);
-            console.log('refreshToken', refreshToken);
-            console.log('profile', profile);
-        }
-    )
-);
-//when use req.body
+// passport.use(
+//     new LineStrategy(
+//         {
+//             channelID: config.LINE_CHANNEL_ID,
+//             channelSecret: config.LINE_CHANNEL_SECRET,
+//             callbackURL:
+//                 'https://shrouded-plains-45055.herokuapp.com/auth/line/callback',
+//         },
+//         function (accessToken, refreshToken, profile, done) {
+//             // asynchronous verification, for effect...
+//             // process.nextTick(function () {
+//             //     // To keep the example simple, the user's LINE profile is returned to
+//             //     // represent the logged-in user.  In a typical application, you would want
+//             //     // to associate the LINE account with a user record in your database,
+//             //     // and return that user instead.
+//             //     return done(null, profile);
+//             // });
+//             console.log('accesToken', accessToken);
+//             console.log('refreshToken', refreshToken);
+//             console.log('profile', profile);
+//         }
+//     )
+// );
+// //when use req.body
 
-app.get('/auth/line', passport.authenticate('line'), function (req, res) {
-    // The request will be redirected to LINE for authentication, so this
-    // function will not be called.
-});
-app.get(
-    '/auth/line/callback',
-    passport.authenticate('line', { failureRedirect: '/login' }),
-    function (req, res) {
-        // res.redirect('/');
-        res.send('success');
-    }
-);
+// app.get('/auth/line', passport.authenticate('line'), function (req, res) {
+//     // The request will be redirected to LINE for authentication, so this
+//     // function will not be called.
+// });
+// app.get(
+//     '/auth/line/callback',
+//     passport.authenticate('line', { failureRedirect: '/login' }),
+//     function (req, res) {
+//         // res.redirect('/');
+//         res.send('success');
+//     }
+// );
 app.use(RestaurantRoute);
 app.listen(config.PORT, () =>
     console.log(`Server RUNNING ON PORT ${config.PORT}`)
--- src/controllers/RestaurantController.js
+++ src/controllers/RestaurantController.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 const Restaurant = require('../models/Restaurant');
 const Foodset = mongoose.model('foodset');
 module.exports.data_get = (req, res) => {
-    res.send('hello');
+    res.render('hello');
 };
