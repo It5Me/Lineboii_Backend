1360dcbdd2495fbc3712fb763b2dc64ac33b03fb
It5Me <56817602+It5Me@users.noreply.github.com>
2021-07-29 16:20:19 +0700

    authorization

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -2,6 +2,6 @@ const { Router } = require('express');
 const router = Router();
 const UserController = require('../controllers/UserController');
 const Authorization = require('../middleware/Authorization');
-router.post('/user', Authorization, UserController.user_post);
+router.get('/user', Authorization, UserController.user_post);
 
 module.exports = router;
