e3f826cbb57733063f15899d0e778f998e60b8f1
It5Me <56817602+It5Me@users.noreply.github.com>
2021-07-29 16:24:09 +0700

    authorization

--- src/middleware/Authorization.js
+++ src/middleware/Authorization.js
@@ -4,15 +4,18 @@ const Authorization = async (req, res, next) => {
     let accessToken;
     // console.log(req.body);
     console.log('Authorization');
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        accessToken = req.headers.authorization.split(' ')[1];
+    try {
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            accessToken = req.headers.authorization.split(' ')[1];
 
-        // console.log('accessToken', accessToken);
-        // req.accessToken = accessToken;
-        next();
-    } else {
-        res.status(401).send('UnAuthorization');
+            // console.log('accessToken', accessToken);
+            // req.accessToken = accessToken;
+            next();
+        }
+    } catch (err) {
+        console.log(err);
     }
+
     try {
         const currentUser = await User.findOne({ accessToken }).populate({
             path: 'profile_id',
