47d3258934dac357cde2d07581cecd1d85854910
It5Me <56817602+It5Me@users.noreply.github.com>
2021-07-30 13:24:48 +0700

    restaurant

--- src/controllers/RestaurantController.js
+++ src/controllers/RestaurantController.js
@@ -3,12 +3,12 @@ const Restaurant = mongoose.model('restaurant');
 
 module.exports.restaurant_home_get = async (req, res) => {
     const user = req.user;
-
+    const limit = req.query.limit;
     try {
         if (!user) {
             return res.send('NoAuthorization');
         }
-        const restaurant = await Restaurant.find({}, { name: 1, restaurantImageURL: 1 });
+        const restaurant = await Restaurant.find({}, { name: 1, restaurantImageURL: 1 }, { limit: limit });
 
         console.log('restaurantData', restaurant);
         res.json(restaurant);
